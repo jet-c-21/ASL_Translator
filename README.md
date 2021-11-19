f1ec4c79cade1c2cf75e44c7f5f0c995990878bc
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2021-11-19 15:27:15 +0800

    defined types of preproc

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
+<style type="text/css">
+    ol { list-style-type: lower-alpha; }
+</style>
+
 # ASL Translator
 
 ![ASL Schematic Diagram](https://i.imgur.com/mq20CVv.png)
 
 ## Dataset
 [dataset1](https://github.com/grassknoted/Unvoiced), [Google Drive](https://drive.google.com/file/d/1FpYkbhAb7fX1z_ygNA1i__av2h6uAkUd/view?usp=sharing)
+
+## Types of preprocessing
+
+#### Base-Norm:
+- Region-Norm : Hand-ROI
+- Illumination-Norm: RGB-to-HSV
+
+#### Data-Aug:
+a. rotate
+b. flip
+c. noise
+d. filter
+e. dilation 
+f. erosion
+z. STL
+    
--- image_pipeline/pipeline.py
+++ image_pipeline/pipeline.py
@@ -3,7 +3,43 @@ from typing import Union
 import numpy as np
 
 from .ult import get_img_ndarray, show_img
-from .preprocessing import fetch_single_hand_roi, rgb_to_hsv
+from .preprocessing import fetch_single_hand_roi, rgb_to_hsv, grayscale, resize
+
+"""
+Types of preprocessing:
+
+Base-Norm:
+    Region Norm : ROI
+    Illumination Norm: RGB -> HSV
+
+Data-Aug:
+    a. rotate
+    b. flip
+    c. noise
+    d. filter
+    e. dilation 
+    f. erosion
+    *. STL
+
+Background-Norm:
+    a. bg_normalization_red_channel()
+    b. bg_normalization_fg_extraction()
+
+Channel-Norm: grayscale
+
+Resolution-Norm: resize
+"""
+
+"""
+Pipeline Naming Convention:
+
+Type-A: this type of pipeline doesn't contain any data augmentation structure
+    pipeline_# : return np.ndarray
+
+Type-B: this type of pipeline contains data augmentation structure
+    pipeline_da_# : return [np.ndarray, ..., np.ndarray]
+
+"""
 
 
 def base_normalize(image: np.ndarray) -> Union[np.ndarray, None]:
@@ -19,8 +55,29 @@ def base_normalize(image: np.ndarray) -> Union[np.ndarray, None]:
     return norm_hand
 
 
-def pipe_1(image: Union[np.ndarray, str]):
+def channel_normalize(image: np.ndarray) -> np.ndarray:
+    return grayscale(image)
+
+
+def resolution_normalize(image: np.ndarray, size=28) -> np.ndarray:
+    return resize(image, size)
+
+
+# >>>>>>>>>>>> pipeline template >>>>>>>>>>>>
+def pipeline_1(image: Union[np.ndarray, str]):
+    """
+    Base-Norm
+
+    Channel-Norm
+
+    Resolution-Norm
+
+    :param image: np.ndarray | str
+    :return:
+    """
     image = get_img_ndarray(image)
     norm_hand = base_normalize(image)
 
     show_img(norm_hand)
+
+# <<<<<<<<<<<< pipeline template <<<<<<<<<<<<
